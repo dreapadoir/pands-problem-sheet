2b8b0959dbafc6b5e8cf2844716e394154a56e9d
David Higgins <G00411302@gmit.ie>
2022-01-28 19:29:37 +0000

    first iteration of bmi.py code

---
+++ bmi.py
@@ -0,0 +1,10 @@
+# bmi.py
+# This program calculates the user's BMI from 2 inputs, height and weight
+# Author: David Higgins
+
+height = float(input("Enter your height in metres: ")) #takes an input and uses float to convert it to an decimal number, assigns value to variable called height
+weight = float(input("Enter your weight in kilograms: ")) #uses same functions as line above to assign a value to variable weight
+
+bmi = weight//height**2         #this calculates the BMI. // is used to get a decimal answer. value returned is assigned to BMI variable
+
+print("Your BMI is {} kg/m^2".format(bmi))  #prints a string that includes the value of the variable bmi
