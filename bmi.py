1346b42069e06b359b78fec32e0d25e2fd362fe6
David Higgins <G00411302@gmit.ie>
2022-02-26 16:11:20 +0000

    Created a function called bmi with arguments height and weight

--- bmi.py
+++ bmi.py
@@ -5,6 +5,7 @@
 height = float(input("Enter your height in metres: ")) #takes an input and uses float to convert it to an decimal number, assigns value to variable called height
 weight = float(input("Enter your weight in kilograms: ")) #uses same functions as line above to assign a value to variable weight
 
-bmi = weight//height**2         #this calculates the BMI. // is used to get a decimal answer. value returned is assigned to BMI variable
-
-print("Your BMI is {} kg/m^2".format(bmi))  #prints a string that includes the value of the variable bmi
+def bmi(height, weight):                #this line defines a function called bmi that will return the calculated answer using height and weight
+    return weight//height**2         #this calculates the BMI. // is used to get a decimal answer.
+           
+print("Your BMI is {} kg/m^2".format(bmi(height,weight)))  #prints a string that includes the value of the variable bmi
