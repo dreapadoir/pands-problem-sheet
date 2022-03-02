ab6484a5224191e4a0c14d50dd893b2d2d1f067b
David Higgins <G00411302@gmit.ie>
2022-03-02 19:44:31 +0000

    removed function using global variables

--- bmi.py
+++ bmi.py
@@ -5,7 +5,6 @@
 height = float(input("Enter your height in metres: ")) #takes an input and uses float to convert it to an decimal number, assigns value to variable called height
 weight = float(input("Enter your weight in kilograms: ")) #uses same functions as line above to assign a value to variable weight
 
-def bmi(height, weight):                #this line defines a function called bmi that will return the calculated answer using height and weight
-    return weight//height**2         #this calculates the BMI. // is used to get a decimal answer.
+bmi = weight//height**2         #this calculates the BMI. // is used to get a decimal answer.
            
 print("Your BMI is {} kg/m^2".format(bmi(height,weight)))  #prints a string that includes the value of the variable bmi
