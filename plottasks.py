5ad9e19e2a38b14888d134d763d4ffd1099cc2a2
David Higgins <G00411302@gmit.ie>
2022-03-14 19:10:47 +0000

    add formatting to plots and function to define x ticks

--- plottasks.py
+++ plottasks.py
@@ -11,8 +11,22 @@ f = xpoints                     #creates f(x) = x
 g = xpoints ** 2                #creates g(x) = x^2
 h = xpoints ** 3                #creates h(x) = x^3
 
-plt.plot(xpoints, f)            #this block of code plots each of the 3 functions on the same graph over the domain defined in xpoints
-plt.plot(xpoints, g)
-plt.plot(xpoints, h)
+names = ['$f(x) = x$','$gx) = x^{2}$', '$h(x) = x^{3}$'] #list of the functions names in TeX
 
+def xtickrange(tickmax):
+    domain = range(0,tickmax)
+    ticklist = [0]
+    while max(ticklist) < max(domain):
+        ticklist.append(max(ticklist) + 0.5)
+
+plt.plot(xpoints, f, 'bo-')            #this block of code plots each of the 3 functions on the same graph over the domain defined in xpoints
+plt.plot(xpoints, g, 'rv--')
+plt.plot(xpoints, h, 'ys-.')
+plt.title('Week 08 task: plottasks.py')
+plt.legend(names)               #creates a legend using the names of the functions typeset in TeX
+plt.xlabel('x values')
+plt.ylabel('y values')
+plt.minorticks_on()
+plt.grid()
+plt.xticks(xtickrange(4))
 plt.show()                      #this line shows the visualisation
