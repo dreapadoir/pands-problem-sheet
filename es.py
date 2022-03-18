707fccc53d75b3c6cadca15f54ef9dbce7e97b30
David Higgins <G00411302@gmit.ie>
2022-03-18 17:40:47 +0000

    added a try except while loop to deal with incorrect values entered at line 33 input prompt

--- es.py
+++ es.py
@@ -9,28 +9,37 @@ filename = sys.argv[1]
 filepaths = []
 
 def readData():
-    if len(filepaths) >= 1:
-        with open(filepaths[choice], "rt") as f:
-            data = f.read()
-            print(data.count("e"))
-    else:
-        print("File not found")
+    with open(filepaths[fchoice], "rt") as f:
+        data = f.read()
+        numberOfEs = data.count("e")
+        print("The number of es in the file is {}".format(numberOfEs))
+
 
 for root, dirs, files in os.walk("/home/", topdown = True):
-   for name in files:
+    for name in files:
         if name == filename:
             filepaths.append(os.path.abspath(os.path.join(root, name)))
 
+
 if len(filepaths) > 1:
     index = 1
+    choicetruth = True
     print("{} results found for {}:".format(len(filepaths),filename))
     for path in filepaths:
         print(index, path)
         index += 1
-    choice = int(input("Enter the number of the search result you want to choose: ")) -1
-    while choice > len(filepaths) or choice < 0:
-        choice = int(input("Please enter a number between 1 and {}: ".format(len(filepaths))))
-elif len(filepaths) <= 1:
-    choice = 0
+    while choicetruth == True:
+        try:
+            choice = input("Enter the number of the search result you want to choose: ")
+            fchoice = int(choice) - 1
+            readData()
+            choicetruth = False 
+        except:
+            print("Error: you need to enter a number between 1 and {}.".format(len(filepaths)))
+    
+elif len(filepaths) == 1:
+    fchoice = 0
+    readData()
 
-dataset = readData()
+elif len(filepaths) == 0:
+    print("File not found")
