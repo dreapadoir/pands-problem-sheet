635798f729c270b80a4ba9852742b0f709c929e5
Higgins <DHiggins@pemnet.com>
2022-03-28 18:14:54 +0100

    add round function to line 8 to display 2 decimal places in final string

--- bmi.py
+++ bmi.py
@@ -5,6 +5,6 @@
 height = float(input("Enter your height in metres: ")) #takes an input and uses float to convert it to an decimal number, assigns value to variable called height
 weight = float(input("Enter your weight in kilograms: ")) #uses same functions as line above to assign a value to variable weight
 
-bmi = weight//height**2         #this calculates the BMI. // is used to get a decimal answer.
+bmi = round(weight/height**2, 2)        #this calculates the BMI.
            
-print("Your BMI is {} kg/m^2".format(bmi(height,weight)))  #prints a string that includes the value of the variable bmi
+print("Your BMI is {} kg/m^2".format(bmi))   #prints a string that includes the value of the variable bmi
