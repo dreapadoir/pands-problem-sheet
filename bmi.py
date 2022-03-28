2dbfd41a49ac2590c99bc1553712e1c619f2fbb6
David Higgins <g00411302@gmit.ie>
2022-03-28 18:22:54 +0100

    add comment to line 8

--- bmi.py
+++ bmi.py
@@ -5,6 +5,6 @@
 height = float(input("Enter your height in metres: ")) #takes an input and uses float to convert it to an decimal number, assigns value to variable called height
 weight = float(input("Enter your weight in kilograms: ")) #uses same functions as line above to assign a value to variable weight
 
-bmi = round(weight/height**2, 2)        #this calculates the BMI.
+bmi = round(weight/height**2, 2)        #this calculates the BMI and rounds the answer to two decimal places
            
 print("Your BMI is {} kg/m^2".format(bmi))   #prints a string that includes the value of the variable bmi
