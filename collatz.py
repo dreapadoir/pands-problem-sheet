4f5d415c99f4bd1a025d39c4abedf61f033ad8c6
David Higgins <g00411302@gmit.ie>
2022-03-29 16:45:08 +0100

    removed the print statement in the while block and added a list to hold the sequence of numbers. this allows the numbers to be printed out on line 18 in the format required for the task

--- collatz.py
+++ collatz.py
@@ -3,8 +3,7 @@
 
 number = int(input("Enter a positive number: ")) #create a variable to store the initial number which will be updated during each iteration of the while block
 index = 0                                        #creates an index number to track how many iterations it takes for the sequence to converge
-
-print("n_{} = {}".format(index,number))         #prints the value of n_0 (the number entered by the user)
+collatzlist = [number]                                #initialise a list to hold the sequence of numbers
 
 while number != 1:                              #initiates a while loop - this code runs as long as number is not equal to 1
     if number % 2 == 0:                         #if statement that checks if number is odd or even
@@ -13,7 +12,8 @@ while number != 1:                              #initiates a while loop - this c
     else:                                       #next block of code runs if the block above is not true                                       
         number = (number * 3) + 1               #multiplies the number by 3 and adds one if current value of number is odd
         index = index + 1                       #increments the index by 1
-    print("n_{} = {}".format(index,number))     #prints out the current iteration and value of number
+    collatzlist.append(number)                  #adds the new value of number to the list
 
 if number == 1:
+    print(collatzlist)                          #prints the full list showing the sequence from the initial number until it converged to 1
     print("Sequence has converged to 1 in {} iterations - program terminated".format(index)) #prints a message stating the program is terminated and how many iterations it took to converge to 1
