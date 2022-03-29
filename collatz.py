ffd1fb5a038902e83d56b4e5ec2af846d5e82a97
David Higgins <g00411302@gmit.ie>
2022-03-29 16:31:23 +0100

    change indentation of print statement in line 17 to make it part of while block instead of else block. Removes the need to have the same statement on line 13. line 13 print statement removed.

--- collatz.py
+++ collatz.py
@@ -10,11 +10,10 @@ while number != 1:                              #initiates a while loop - this c
     if number % 2 == 0:                         #if statement that checks if number is odd or even
         number = number // 2                    #divides number by 2 if it is even
         index = index + 1                       #increments the index by 1
-        print("n_{} = {}".format(index,number)) #prints out the current iteration and value of number
     else:                                       #next block of code runs if the block above is not true                                       
         number = (number * 3) + 1               #multiplies the number by 3 and adds one if current value of number is odd
         index = index + 1                       #increments the index by 1
-        print("n_{} = {}".format(index,number)) #prints out the current iteration and value of number
+    print("n_{} = {}".format(index,number))     #prints out the current iteration and value of number
 
 if number == 1:
     print("Sequence has converged to 1 in {} iterations - program terminated".format(index)) #prints a message stating the program is terminated and how many iterations it took to converge to 1
